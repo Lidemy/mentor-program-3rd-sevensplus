626fbf3dd73eb976e4c0d1e1f81284784d3c4b3c
unknown <kebcdy@gmail.com>
2019-04-20 09:30:26 +0800

    'v2'

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,5 +1,16 @@
 function alphaSwap(str) {
-  console.log(str);
+  const StrCopy = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] >= 'a' && str[i] <= 'z') {
+      StrCopy[i] = StrCopy[i].toUpperCase();
+    } else if (str[i] >= 'A' && str[i] <= 'Z') {
+      StrCopy[i] = StrCopy[i].toLowercase();
+    } else {
+      StrCopy[i] = str[i];
+    }
+  }
+  return (StrCopy);
 }
 
+alphaSwap('HellO');
 module.exports = alphaSwap;
--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,5 +1,14 @@
 function isPrime(n) {
-  console.log(n);
+  let num = 0;
+  for (let i = 1; i <= n; i += 1) {
+    if (n % i === 0) {
+      num += 1;
+    }
+  }
+  if (num === 2) {
+    return (true);
+  }
+  return (false);
 }
 
 module.exports = isPrime;
