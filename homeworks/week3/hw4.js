8d17e639e78da2562ddf52bd0f4f962135ee1ffb
unknown <kebcdy@gmail.com>
2019-04-20 10:43:04 +0800

    'v2.2'

--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,5 +1,21 @@
+/* version 1
+
 function isPalindromes(str) {
-  console.log(str);
+  const final = str.length - 1;
+  for (let i = 0; i <= str.length / 2 ; i += 1) {
+    if (str[i] !== str[final - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+*/
+
+// version 2
+function isPalindromes(str) {
+  const rev = str.split('').reverse().join('');
+  return rev === str;
 }
 
 module.exports = isPalindromes;
