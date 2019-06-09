ebe26e1bdf17b026e4af40fa97e8394837ada4a7
unknown <kebcdy@gmail.com>
2019-06-09 20:30:06 +0800

    'ver2.1'

--- homeworks/week8/hw1/script.js
+++ homeworks/week8/hw1/script.js
@@ -13,43 +13,50 @@ document.querySelector('.lotteryTime').addEventListener('click',
         const background__sel = document.querySelector('body');
         const text = document.querySelector('.awards');
 
-        if (gift === 'FIRST') {
-          text.innerHTML = '恭喜你中頭獎了！<br/>日本東京來回雙人遊！！！';
-          background__sel.style.background = 'skyblue';
-          pic__sel.style.background = 'url("https://images.pexels.com/photos/1004584/pexels-photo-1004584.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940") no-repeat top left';
-          pic__sel.style['background-size'] = 'contain';
+        switch (gift) {
+          case 'FIRST':
+            text.innerHTML = '恭喜你中頭獎了！<br/>日本東京來回雙人遊！！！';
+            background__sel.style.background = 'skyblue';
+            pic__sel.style.background = 'url("https://images.pexels.com/photos/1004584/pexels-photo-1004584.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940") no-repeat top left';
+            break;
 
-        } else if (gift === 'SECOND') {
-          text.innerHTML = '二獎！<br/>90吋電視一台';
-          background__sel.style.background = 'moccasin';
-          pic__sel.style.background = 'url("https://images.pexels.com/photos/987586/pexels-photo-987586.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940") no-repeat center center';
-          pic__sel.style['background-size'] = 'contain';
+          case 'SECOND':
+            text.innerHTML = '二獎！<br/>90吋電視一台';
+            background__sel.style.background = 'moccasin';
+            pic__sel.style.background = 'url("https://images.pexels.com/photos/987586/pexels-photo-987586.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940") no-repeat center center';
+            break;
 
-        } else if (gift === 'THIRD') {
-          text.innerHTML = '恭喜你抽中三獎！<br/>知名 Youtube 簽名握手會入場券一張，bang！';
-          background__sel.style.background = 'mediumaquamarine';
-          pic__sel.style.background = 'url("https://doqvf81n9htmm.cloudfront.net/data/crop_article/87021/0213-1140-1.jpg_1140x855.jpg") no-repeat center center';
-          pic__sel.style['background-size'] = 'contain';
+          case 'THIRD':
+            text.innerHTML = '恭喜你抽中三獎！<br/>知名 Youtube 簽名握手會入場券一張，bang！';
+            background__sel.style.background = 'mediumaquamarine';
+            pic__sel.style.background = 'url("https://doqvf81n9htmm.cloudfront.net/data/crop_article/87021/0213-1140-1.jpg_1140x855.jpg") no-repeat center center';
+            break;
 
-        } else if (gift === 'NONE') {
-          text.innerHTML = '銘謝惠顧';
-          text.style['font-size'] = '32pt';
-          text.style.color = 'white';
-          text.style.position = 'relative';
-          text.style.top = '25%';
-          background__sel.style.background = 'black';
-        } else {
-          alert('系統不穩定，再試一次');
+          case 'NONE':
+            text.innerHTML = '銘謝惠顧';
+            text.style['font-size'] = '32pt';
+            text.style.color = 'white';
+            text.style.position = 'relative';
+            text.style.top = '25%';
+            background__sel.style.background = 'black';
+            break;
+
+          default:
+            alert('系統不穩定，再試一次');
+            break;
         }
+
+        pic__sel.style['background-size'] = 'contain';
         document.querySelector('main').style.visibility = 'visible';
+
       } else {
           alert('系統不穩定，再試一次');
       }
     }
     request.send();
   }
-)
+);
 
 document.querySelector('.end').addEventListener('click', function() {
   location.reload();
-  });
+});
--- homeworks/week8/hw2/script.js
+++ homeworks/week8/hw2/script.js
@@ -1,9 +1,10 @@
 const request = new XMLHttpRequest();
 
-request.open('GET', 'https://lidemy-book-store.herokuapp.com/posts?_limit=20&_sort=id&_order=desc',true);
+request.open('GET', 'https://lidemy-book-store.herokuapp.com/posts?_limit=20&_sort=id&_order=desc', true);
 
 request.onload = function() {
   const info = JSON.parse(this.responseText);
+
   for (let i = 0; i < 20; i += 1) {
     const text = document.createElement('div');
     text.setAttribute('class', `id__${i + 1}`);
@@ -14,11 +15,19 @@ request.onload = function() {
 
 request.send();
 
-document.querySelector('.submits').addEventListener('click',
-  function() {
-    const postText = document.querySelector("input[class='words']").value;
-    request.open('POST', `https://lidemy-book-store.herokuapp.com/posts?content=${postText}`, true);
-    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    request.send(`content=${postText}`);
-    location.reload();
-  });
+document.querySelector('.submits').onclick = () =>{
+  const postText = document.querySelector("input[class='words']").value;
+
+  request.open('POST', 'https://lidemy-book-store.herokuapp.com/posts', true);
+  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  request.send(`content=${postText}`);
+
+  request.onload = () => {
+    if (request.status >= 200 && request.status < 400) {
+      const text = document.createElement('div');
+      text.innerText = postText;
+      document.querySelector('.oldPost').prepend(text);
+      document.querySelector("input[class='words']").value = '';
+    }
+  };
+};
