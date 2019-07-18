d0ca351369add7ff36f729ae576059827a9fde69
unknown <kebcdy@gmail.com>
2019-07-18 11:53:02 +0800

    week13 -- version 1.2

--- homeworks/week13/hw2/script.js
+++ homeworks/week13/hw2/script.js
@@ -1,17 +1,17 @@
-function count(){
+function count() {
     $('.number').text($('ul>li').length);
 }
 
 /*  新增留言  */
 let piece_num = 0;
-$(".new-td").keydown( (event) => {
+$('.new-td').keydown( (event) => {
     if(event.keyCode !== 13 || $("input[class='new-td']").val() == "") {
-        return ;
+        return;
     }
-    jQuery('<li/>',{
+    jQuery('<li/>', {
         id:`piece${piece_num}`,
-        "class":'pieces active',
-        text:$("input[class='new-td']").val()
+        'class':'pieces active',
+        text: $("input[class='new-td']").val()
     }).appendTo('.td-list');
     $("input[class='new-td']").val("");
     piece_num += 1;
@@ -19,9 +19,9 @@ $(".new-td").keydown( (event) => {
 })
 
 /*  選取條目  */
-$('ul').delegate('li','click',choose);
-function choose(a){
-    let item = $(`#${a.target.id}`);
+$('ul').delegate('li', 'click', choose);
+function choose(a) {
+    const item = $(`#${a.target.id}`);
     if (item.hasClass('choose')) {
         item.removeClass('choose');
     } else {
@@ -30,23 +30,23 @@ function choose(a){
 }
 
 /*  類別監聽  */
-$('.function').delegate('button','click',isDone);
+$('.function').delegate('button', 'click', isDone);
 function isDone(a){
-    if(a.target.id == "active_list"){
-        $('.completed').css('display','none');
-        $('.active').css('display','list-item');
+    if (a.target.id == "active_list") {
+        $('.completed').css('display', 'none');
+        $('.active').css('display', 'list-item');
     } else if (a.target.id == "complete_list"){
-        $('.completed').css('display','list-item');
-        $('.active').css('display','none');
+        $('.completed').css('display', 'list-item');
+        $('.active').css('display', 'none');
     } else {
-        $('.completed').css('display','list-item');
-        $('.active').css('display','list-item');
+        $('.completed').css('display', 'list-item');
+        $('.active').css('display', 'list-item');
     }
 }
 
 /*  動作監聽  */
-$('.function').delegate('img','click',action);
-function action(a){
+$('.function').delegate('img', 'click', action);
+function action(a) {
     if(a.target.id == "complete") {
         $('.choose').addClass('completed');
         $('.choose').removeClass('active');
--- homeworks/week13/hw3/script.js
+++ homeworks/week13/hw3/script.js
@@ -8,39 +8,39 @@ function add_post(a) {
     if ($(`.reply_content_${a.target.id}`).val() == "") {
         return;
     }
-    let url = "./handleNewPost.php";
+    const url = "./handleNewPost.php";
     let formData = new FormData();
-    formData.append("new_messager", $(`textarea[class='reply_content_${a.target.id}']`).val());
-    formData.append("parent_id", a.target.id);
+    formData.append('new_messager', $(`textarea[class='reply_content_${a.target.id}']`).val());
+    formData.append('parent_id', a.target.id);
     fetch(url, {
         method: 'POST',
         body: formData,
     }).then((res) => {
         return res.json();
     }).then((res) => {
-        let insert_id = res.id;
-        if (a.target.id == 0) {  // 判斷是不是主留言
+        const insert_id = res.id;
+        if (a.target.id == 0) {  //判斷是不是主留言
             let new_piece = `pie_${insert_id}`
-            $('.list_top').after(jQuery(`<div/>`, { class: `pieces ${new_piece}`, text: ''}));
+            $('.list_top').after(jQuery(`<div/>`, { class: `pieces ${new_piece}`, text: '' }));
             $(`.${new_piece}`).css('display','none');
-            create('div', 'author', res.nickname,`.${new_piece}`);
+            create('div', 'author', res.nickname, `.${new_piece}`);
             create('div', 'content', $(`textarea[class='reply_content_${a.target.id}']`).val(),`.${new_piece}`);
-            create('div', 'time', res.time,`.${new_piece}`);
+            create('div', 'time', res.time, `.${new_piece}`);
             jQuery(`<a/>`, { href: `./changePost.php?mess_id=${insert_id}&path=index`, text: "編輯留言" }).appendTo(`.${new_piece}`);
             jQuery(`<a/>`, { id: `del_${insert_id}`, class: 'del_button', text: '刪除留言' }).appendTo(`.${new_piece}`);
 
             create('div', 'reply_box', '',`.${new_piece}`);
             jQuery(`<textarea/>`, { name: 'new_messager', class: `reply_content_${insert_id}` }).appendTo(`.${new_piece} > .reply_box`);
-            jQuery(`<input/>`, { type: 'submit', class: `post_button`, id: `${insert_id}` }).appendTo(`.${new_piece} > .reply_box`);
+            jQuery(`<input/>`, { type: 'submit', class: 'post_button', id: `${insert_id}` }).appendTo(`.${new_piece} > .reply_box`);
             $(`.${new_piece}`).show(500);
         } else {
             let new_piece = `sub_pie_${insert_id}`;
-            $(`.pie_${a.target.id} > .reply_box`).before(jQuery(`<div/>`, { class: `sub_pieces ${new_piece}`, text: ''}));
+            $(`.pie_${a.target.id} > .reply_box`).before(jQuery(`<div/>`, { class: `sub_pieces ${new_piece}`, text: '' }));
             $(`.${new_piece}`).css('display','none');
             if ($(`.pie_${a.target.id} > a`).length > 0) {
                 $(`.${new_piece}`).addClass('author_reply');
             }
-            create('div', 'sub_author', res.nickname,`.${new_piece}`);
+            create('div', 'sub_author', res.nickname, `.${new_piece}`);
             create('div', 'sub_content', $(`textarea[class='reply_content_${a.target.id}']`).val(),`.${new_piece}`);
             create('div', 'sub_time', res.time,`.${new_piece}`);
             jQuery(`<a/>`, { href: `./changePost.php?mess_id=${insert_id}&path=index`, text: '編輯留言' }).appendTo(`.${new_piece}`);
@@ -56,10 +56,10 @@ $('.index__main').on('click', '.post_button', add_post);
 
 /* 刪除留言 */
 function del_post(a) {
-    let conf = confirm('是否刪除留言');
+    const conf = confirm('是否刪除留言');
     if (conf == true) {
-        let mess_id = a.target.id.split('_')[1];
-        let url = `./handleDeletePost.php?mess_id=${mess_id}`;
+        const mess_id = a.target.id.split('_')[1];
+        const url = `./handleDeletePost.php?mess_id=${mess_id}`;
         event.preventDefault();
         fetch(url)
             .then((res) => {
