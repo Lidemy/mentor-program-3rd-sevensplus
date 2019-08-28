b2df95a41ab1aedf95df034170051d3ee5b735e1
unknown <kebcdy@gmail.com>
2019-08-28 17:56:08 +0800

    ver 1

---
+++ homeworks/week19/hw1/readme.md
@@ -0,0 +1,9 @@
+Base URL: https://www.sevvenplus.com/todoList/API
+
+| 說明            | Method | path           | 參數                         | 範例             |
+|---------------- |--------|---------------|------------------------------|------------------|
+| 獲取所有代辦事項 | GET    | /lists        | 無                            |                 |
+| 獲取單一事項     | GET    | /lists/:id    | 無                            | /lists/10       |
+| 新增事項        | POST    | /lists        | text: 待辦事項                | 無              |
+| 刪除事項        | DELETE  | /lists/:id    | 無                           | 無               |
+| 修改狀態        | PATCH   | /lists/:id    | type：完成狀態 | 無               |
---
+++ homeworks/week19/hw2/script.js
@@ -0,0 +1,135 @@
+fetch('https://www.sevvenplus.com/todoList/API/lists', {
+  method: 'GET'
+}).then((res) => {
+  return res.json();
+}).then((res) => {
+  for (let i = 0; i < res.length; i += 1) {
+    const item = res[i];
+    jQuery('<li/>', {
+      id: item.id,
+      'class': `pieces ${item.type}`,
+      text: item.text
+    }).appendTo('.td-list');
+  }
+  count();
+}).catch((err) => {
+  console.log(err);
+});
+
+//  新增留言
+$('.new-td').keydown((event) => {
+  if (event.keyCode !== 13 || $("input[class='new-td']").val() == '') return;
+  const text = $("input[class='new-td']").val();
+  $("input[class='new-td']").val('');
+  fetch('https://www.sevvenplus.com/todoList/API/lists', {
+    method: 'POST',
+    body: JSON.stringify({ 'text': text })
+  }).then((res) => {
+    return res.json();
+  }).then((res) => {
+    if (res.mode == 'successful') {
+      jQuery('<li/>', {
+        id: res.id,
+        'class': 'pieces active',
+        'text':  text
+      }).appendTo('.td-list');
+    }
+    count();
+  }).catch((err) => {
+    console.log(err);
+  })
+});
+
+//  選取條目
+$('ul').delegate('li', 'click', choose);
+function choose(a) {
+  const item = $(`#${a.target.id}`);
+  if (item.hasClass('choose')) {
+    item.removeClass('choose');
+  } else {
+    item.addClass('choose');
+  }
+}
+
+//  類別監聽
+$('.function').delegate('button', 'click', isDone);
+function isDone(a) {
+  viewer(a.target.id);
+}
+
+//  動作監聽
+$('.function').delegate('img', 'click', action);
+function action(a) {
+  const item = $('.choose');
+  for (let i = 0; i < item.length; i += 1 ) {
+    const num = parseInt(item[i].id, 10);
+    if (a.target.id == 'delete') {
+      deleted(num);
+    } else if ($(`#${num}`).hasClass('completed')) {
+      changeType(num, 'active', 'completed');
+    } else {
+      changeType(num, 'completed', 'active');
+    }
+  }
+  $('.choose').removeClass('choose');
+  count();
+}
+function deleted(a) {
+  fetch(`https://www.sevvenplus.com/todoList/API/lists/${a}`, {
+    method: 'DELETE',
+  }).then((res) => {
+    return res.json();
+  }).then((res) => {
+    if (res.mode == 'successful') {
+      $(`#${a}`).remove();
+      count();
+    }
+  }).catch((err) => {
+    console.log(err)
+  })
+};
+function changeType(a, b, c) {
+  fetch(`https://www.sevvenplus.com/todoList/API/lists/${a}`, {
+    method: 'PATCH',
+    body: JSON.stringify({ 'type': b })
+  }).then((res) => {
+    return res.json();
+  }).then((res) => {
+    if (res.mode == 'successful') {
+      $(`#${a}`).removeClass(c);
+      $(`#${a}`).addClass(b);
+    }
+  }).catch((err) => {
+    console.log(err)
+  })
+};
+//  畫面顯示相關
+function count() {
+  $('.number').text($('ul>li').length);
+};
+function countType(a) {
+  $('.number').text($(`.${a}`).length);
+};
+function viewer(view) {
+  if (view == "active") {
+    visibility('active');
+    countType('active');
+  } else if (view == 'completed') {
+    visibility('completed');
+    countType('completed');
+  } else {
+    $('li').css('display', 'list-item');
+    $('li').removeClass('first');
+    $('li').removeClass('last');
+    count();
+  }
+}
+function visibility(a) {
+  $('li').css('display', 'none');
+  $(`.${a}`).css('display', 'list-item');
+  const num = $(`.${a}`);
+  const fir = num[0].id;
+  const last = num[num.length - 1].id;
+  $(`#${fir}`).addClass('first');
+  $(`#${last}`).addClass('last');
+}
