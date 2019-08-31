7505f4f90c31ceeddcd84cff30afe096c468f9b4
unknown <kebcdy@gmail.com>
2019-08-31 18:24:37 +0800

    ver 1.3

--- homeworks/week19/hw1/index.php
+++ homeworks/week19/hw1/index.php
@@ -6,7 +6,7 @@ $utils = new utilsFunction;
 
 header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Methods:POST, GET, PUT, OPTIONS, PATCH, DELETE');
-header('Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept, Authorization');
+header('Access-Control-Allow-Headers:Origin, X-Requested-With, Content-Type, Accept, Authorization');
 header('Access-Control-Allow-Credentials:true');
 
 if($method == 'GET'){
